d0efd1f0a8cff41e5b7405fce38cd1e2c070f0a0
sripriya7-ande <sripriya7-ande@outlook.com>
2025-06-24 16:15:07 +0530

    added new file

--- inventory/generate_inventory.sh
+++ inventory/generate_inventory.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-terraform output -raw frontend_ip > frontend_ip.txt
-terraform output -raw backend_ip > backend_ip.txt
 
-FRONTEND_IP=$(cat frontend_ip.txt)
-BACKEND_IP=$(cat backend_ip.txt)
+cd ../terraform
 
-cat > inventory.ini <<EOF
+FRONTEND_IP=$(terraform output -raw frontend_ip)
+BACKEND_IP=$(terraform output -raw backend_ip)
+
+cd ../inventory
+
+cat <<EOF > inventory.ini
 [frontend]
-c8.local ansible_host=$FRONTEND_IP ansible_user=ec2-user
+c8 ansible_host=$FRONTEND_IP ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/New.pem
 
 [backend]
-u21.local ansible_host=$BACKEND_IP ansible_user=ubuntu
+u21 ansible_host=$BACKEND_IP ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/New.pem
 EOF
+
