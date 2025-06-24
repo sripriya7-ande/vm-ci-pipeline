45d1bc331072fbe48fef6c8193b8c420a93177e6
sripriya7-ande <sripriyaande@outlook.com>
2025-06-24 17:33:57 +0530

    Update generate_inventory.sh

--- inventory/generate_inventory.sh
+++ inventory/generate_inventory.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Get outputs from Terraform
-cd terraform || exit 1
+cd terraform
 frontend_ip=$(terraform output -raw frontend_ip)
 backend_ip=$(terraform output -raw backend_ip)
+cd - > /dev/null
 
-# Go to inventory directory to write inventory.ini
-cd ../inventory || exit 1
+PRIVATE_KEY_PATH="/var/lib/jenkins/.ssh/New.pem"
 
-# Create inventory.ini
-cat > inventory.ini <<EOF
+cat > inventory/inventory.ini <<EOF
 [frontend]
-c8 ansible_host=${frontend_ip} ansible_user=ec2-user ansible_ssh_private_key_file=\${ANSIBLE_PRIVATE_KEY:-~/.ssh/New.pem}
+c8 ansible_host=${frontend_ip} ansible_user=ec2-user ansible_ssh_private_key_file=${PRIVATE_KEY_PATH}
 
 [backend]
-u21 ansible_host=${backend_ip} ansible_user=ubuntu ansible_ssh_private_key_file=\${ANSIBLE_PRIVATE_KEY:-~/.ssh/New.pem}
+u21 ansible_host=${backend_ip} ansible_user=ubuntu ansible_ssh_private_key_file=${PRIVATE_KEY_PATH}
 EOF
-
